5b1095b3572449c9bbd93a3d358bb91183646983
Guilherme B. Pinheiro <guilhermepinheiro@gmail.com>
2019-02-20 19:19:27 +0000

    Diagonal Difference. 👍

---
+++ src/algorithms/warmup/DiagonalDifference.java
@@ -0,0 +1,62 @@
+package algorithms.warmup;
+
+import java.io.*;
+        import java.math.*;
+        import java.security.*;
+        import java.text.*;
+        import java.util.*;
+        import java.util.concurrent.*;
+        import java.util.regex.*;
+
+public class DiagonalDifference {
+
+    // Complete the diagonalDifference function below.
+    static int diagonalDifference(int[][] arr) {
+
+        int diagSum1 = 0;
+        int diagSum2 = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            diagSum1 += arr[i][i];
+            diagSum2 += arr[arr.length - 1 - i][i];
+        }
+
+        if (diagSum1 > diagSum2) {
+            return diagSum1 - diagSum2;
+        }
+        else {
+            return  diagSum2 - diagSum1;
+        }
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[][] arr = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < n; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+
+        int result = diagonalDifference(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
